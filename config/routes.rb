fecdfde5d49385468041263a7b9c138aca4c7879
Nick Powell <nickpowells@gmail.com>
2017-07-07 12:59:18 -0700

    add review form and submit

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     resources :products
   end
 
+  resources :products do
+    resources :reviews
+  end
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
