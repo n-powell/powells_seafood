794c38930dbaf91bba7984a677a2ab77879cb9e2
Nick Powell <nickpowells@gmail.com>
2017-07-07 12:25:41 -0700

    add edit and show views + edit/delete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,6 +16,33 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    @user = current_user
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @product = @user.products.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to user_products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to user_products_path
   end
 
   private
