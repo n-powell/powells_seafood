fe8fe866a8dd15b2be749a544f3f4b91bf3443ec
Nick Powell <nickpowells@gmail.com>
2017-07-07 11:49:11 -0700

    add products route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root to: "products#index"
   devise_for :users
+
+  resources :user do
+    resources :products
+  end
+
+  resources :products
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
