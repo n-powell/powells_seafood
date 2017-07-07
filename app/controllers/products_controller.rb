8261523e898658ab645b6bc8450889cf663879c5
Nick Powell <nickpowells@gmail.com>
2017-07-07 11:26:14 -0700

    add product and review models

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,26 @@
 class ProductsController < ApplicationController
+  def new
+    @user = current_user
+    @product = @user.products.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @product = @user.products.new(product_params)
+    if @product.save
+      redirect_to user_products_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :name)
+  end
 end
