7e4f51657068188c97e8c89a634e9f7a519f6930
Nick Powell <nickpowells@gmail.com>
2017-07-07 16:30:48 -0700

    add products scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   has_many :reviews
   belongs_to :user
   validates :image, :title, :description, :price, presence: true
+
+  scope :alph_order, -> { order(title: :asc) }
 end
