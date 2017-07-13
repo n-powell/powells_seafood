236a4ead367e5962d91e91dd7515ad0557088f11
Nick Powell <nickpowells@gmail.com>
2017-07-13 00:59:31 -0700

    add styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,28 @@ end
                         password: "password")
 end
 
-20.times do |index|
+5.times do |index|
   Product.create!(title: "Pacific Rock Crab",
                         image: "crab.jpg",
                         price: "12",
+                        description: "Romaleon antennarium has a fan-shaped carapace with eleven teeth to either side of the eyestalks, the widest point falling at the eighth or ninth tooth. The chelipeds are quite stout with the black tips bent downward. The antennae are long and prominent, accounting for the specific name. The dorsal surfaces of adults are uniformly red, but the ventral surface of the carapace is spotted.
+
+This species is easily confused with the red rock crab, Cancer productus. They can be distinguished by the less prominent antennae, less robust claws, and lack of ventral spots on the latter.")
+end
+
+1.times do |index|
+  Product.create!(title: "Halibut fillets",
+                        image: "halibut.png",
+                        price: "4.50",
+                        description: "Romaleon antennarium has a fan-shaped carapace with eleven teeth to either side of the eyestalks, the widest point falling at the eighth or ninth tooth. The chelipeds are quite stout with the black tips bent downward. The antennae are long and prominent, accounting for the specific name. The dorsal surfaces of adults are uniformly red, but the ventral surface of the carapace is spotted.
+
+This species is easily confused with the red rock crab, Cancer productus. They can be distinguished by the less prominent antennae, less robust claws, and lack of ventral spots on the latter.")
+end
+
+1.times do |index|
+  Product.create!(title: "Maine Lobster",
+                        image: "lobster.jpg",
+                        price: "26",
                         description: "Romaleon antennarium has a fan-shaped carapace with eleven teeth to either side of the eyestalks, the widest point falling at the eighth or ninth tooth. The chelipeds are quite stout with the black tips bent downward. The antennae are long and prominent, accounting for the specific name. The dorsal surfaces of adults are uniformly red, but the ventral surface of the carapace is spotted.
 
 This species is easily confused with the red rock crab, Cancer productus. They can be distinguished by the less prominent antennae, less robust claws, and lack of ventral spots on the latter.")
