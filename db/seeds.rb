7641c66d576d81b55a8c4b5abc67b39595bfc129
Nick Powell <nickpowells@gmail.com>
2017-07-13 01:11:47 -0700

    add seed options

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ end
 5.times do |index|
   Product.create!(title: "Pacific Rock Crab",
                         image: "crab.jpg",
-                        price: "12",
+                        price: "12.55",
                         description: "Romaleon antennarium has a fan-shaped carapace with eleven teeth to either side of the eyestalks, the widest point falling at the eighth or ninth tooth. The chelipeds are quite stout with the black tips bent downward. The antennae are long and prominent, accounting for the specific name. The dorsal surfaces of adults are uniformly red, but the ventral surface of the carapace is spotted.
 
 This species is easily confused with the red rock crab, Cancer productus. They can be distinguished by the less prominent antennae, less robust claws, and lack of ventral spots on the latter.")
@@ -26,6 +26,33 @@ end
   Product.create!(title: "Halibut fillets",
                         image: "halibut.png",
                         price: "4.50",
+                        description: "Romaleon antennarium has a fan-shaped carapace with eleven teeth to either side of the eyestalks, the widest point falling at the eighth or ninth tooth. The chelipeds are quite stout with the black tips bent downward. The antennae are long and prominent, accounting for the specific name. The dorsal surfaces of adults are uniformly red, but the ventral surface of the carapace is spotted.
+
+This species is easily confused with the red rock crab, Cancer productus. They can be distinguished by the less prominent antennae, less robust claws, and lack of ventral spots on the latter.")
+end
+
+2.times do |index|
+  Product.create!(title: "Luisianna catfish fillets",
+                        image: "halibut.png",
+                        price: "4.50",
+                        description: "Romaleon antennarium has a fan-shaped carapace with eleven teeth to either side of the eyestalks, the widest point falling at the eighth or ninth tooth. The chelipeds are quite stout with the black tips bent downward. The antennae are long and prominent, accounting for the specific name. The dorsal surfaces of adults are uniformly red, but the ventral surface of the carapace is spotted.
+
+This species is easily confused with the red rock crab, Cancer productus. They can be distinguished by the less prominent antennae, less robust claws, and lack of ventral spots on the latter.")
+end
+
+1.times do |index|
+  Product.create!(title: "Large Pacific Shrimp",
+                        image: "shrimp.jpg",
+                        price: "14.50",
+                        description: "Romaleon antennarium has a fan-shaped carapace with eleven teeth to either side of the eyestalks, the widest point falling at the eighth or ninth tooth. The chelipeds are quite stout with the black tips bent downward. The antennae are long and prominent, accounting for the specific name. The dorsal surfaces of adults are uniformly red, but the ventral surface of the carapace is spotted.
+
+This species is easily confused with the red rock crab, Cancer productus. They can be distinguished by the less prominent antennae, less robust claws, and lack of ventral spots on the latter.")
+end
+
+1.times do |index|
+  Product.create!(title: "Bay Oysters",
+                        image: "oysters.jpeg",
+                        price: "11.75",
                         description: "Romaleon antennarium has a fan-shaped carapace with eleven teeth to either side of the eyestalks, the widest point falling at the eighth or ninth tooth. The chelipeds are quite stout with the black tips bent downward. The antennae are long and prominent, accounting for the specific name. The dorsal surfaces of adults are uniformly red, but the ventral surface of the carapace is spotted.
 
 This species is easily confused with the red rock crab, Cancer productus. They can be distinguished by the less prominent antennae, less robust claws, and lack of ventral spots on the latter.")
