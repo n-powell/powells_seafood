4dc36ffff725a7f122bcd2ba0391239a8701d469
Nick Powell <nickpowells@gmail.com>
2017-07-13 21:10:34 -0700

    add redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, :only => [:new, :edit]
+  before_action :authenticate_user!, :only => [:new, :edit] do
+    redirect_to new_user_session_path unless current_user && current_user.admin
+  end
   helper_method :is_admin?
 
   def is_admin?
